6699ab25a3d4379dab4de6d5d3e18c3fe4029059
Richard Keller <richard@briefmedia.com>
2017-04-21 10:56:12 -0500

    ssl context

--- getone.py
+++ getone.py
@@ -1,44 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# db
-import mysql.connector
-
-config = {
-  'user': 'root',
-  'password': '',
-  'host': '127.0.0.1',
-  'database': 's1',
-  'raise_on_warnings': True,
-  'use_pure': False,
-}
-cnx = mysql.connector.connect(**config)
-cursor = cnx.cursor()
-
-
-print cursor
-
-# time
+import ssl
+import smtplib
+import re
 from time import localtime, strftime
-
-# cgi, to get params
 import cgi
-
-# enable debugging
 import cgitb
 cgitb.enable()
+# from urllib import request
+from urllib.request import Request, urlopen
 
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 # html parser
 # https://docs.python.org/2/library/htmlparser.html
 # from HTMLParser import HTMLParser
 
-# url getter
-import urllib2
+# url 
+url = 'https://www.cliniciansbrief.com/'
+context = ssl._create_unverified_context()
+ssl._create_default_https_context = ssl._create_unverified_context
+headers={'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}
+response = Request(url, headers=headers)
+html = urlopen(response).read()
+html = re.escape(html)
 
-response = urllib2.urlopen('https://mattsatv.com/')
-html = response.read()
+
+# db.query("insert into submissions (url, html) values ('" + url + "', '" + html + "')")
 
 # soup = BeautifulSoup(html, 'html.parser')
 # print(soup.prettify())
@@ -50,9 +39,10 @@ html = response.read()
     # print(link)
 
 # print(soup.get_text())
-filename = 'test.html'
-target = open(filename, 'w')
-target.write('test')
+
+# file write example
+## filename = 'test.html'
+## target = open(filename, 'w')
+## target.write('test')
 
 
-cnx.close()
