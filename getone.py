02ddaa4ba62f7bd97fb0355be0c6e254e94a4209
Richard Keller <richard@briefmedia.com>
2017-04-21 14:41:55 -0500

    sqlite

--- getone.py
+++ getone.py
@@ -8,23 +8,50 @@ from time import localtime, strftime
 import cgi
 import cgitb
 cgitb.enable()
-# from urllib import request
 from urllib.request import Request, urlopen
+import sqlite3
+dbname = 'search.db'
+print('>>> Connecting to SQLITE database: ' + dbname)
+conn = sqlite3.connect(dbname)
+print('>>> Creating cursor')
+c = conn.cursor()
 
-# from bs4 import BeautifulSoup
+submission_table = '''create table IF NOT EXISTS submissions (
+	id int auto_increment primary key,
+	url varchar(255) not null,
+	html longtext not null
+)'''
 
-# html parser
-# https://docs.python.org/2/library/htmlparser.html
-# from HTMLParser import HTMLParser
+print('>>> Running SQL: ' + submission_table)
+c.execute(submission_table)
+conn.commit()
 
-# url 
-url = 'https://www.cliniciansbrief.com/'
+# url
+url1 = 'http://www.useragentstring.com/pages/useragentstring.php'
+url = 'http://www.useragentstring.com/pages/useragentstring.php?name=Accoona-AI-Agent'
 context = ssl._create_unverified_context()
 ssl._create_default_https_context = ssl._create_unverified_context
-headers={'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}
+
+agent1 = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+agent = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+headers={'User-Agent': agent1}
 response = Request(url, headers=headers)
 html = urlopen(response).read()
 html = re.escape(html)
+
+# html = 'test'
+
+c.execute('INSERT INTO submissions (url, html) VALUES ("'+url+'", "'+html+'")')
+conn.commit()
+
+
+
+conn.close()
+print('>>> Closing DB connection')
+print(strftime("%a, %d %b %Y %H:%M:%S", localtime()))
+
+
+
 
 
 # db.query("insert into submissions (url, html) values ('" + url + "', '" + html + "')")
